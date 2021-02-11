a7ef9e7ccb17e0f9f1fd353fa7e70e08c66241ba
dai-gi <daisuke.u524@gmail.com>
2021-02-12 06:43:06 +0900

    画像投稿機能を追加

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Post(models.Model):
+    image = models.ImageField('画像', upload_to='img', null=True, blank=True)
     title = models.CharField("タイトル", max_length=100)
     content = models.TextField("内容")
 
