de70f1c04a31c3d644e9eaea448a339d5298e10c
dai-gi <daisuke.u524@gmail.com>
2021-02-19 07:57:39 +0900

    Iconモデルを追加

--- myapp/models.py
+++ myapp/models.py
@@ -8,3 +8,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Icon(models.Model):
+    image = models.ImageField('イメージ画像', upload_to='img', null=True, blank=True)
+    name = models.CharField("タイトル", max_length=20)
+    comment = models.TextField("内容")
+
+    def __str__(self):
+        return self.name
+
