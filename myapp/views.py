0fafbf02116559d925b95771ab39bb04739c3903
dai-gi <daisuke.u524@gmail.com>
2021-02-19 06:46:36 +0900

    詳細画面ついか

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
 
@@ -9,6 +9,14 @@ class IndexView(View):
         post_data = Post.objects.order_by("-id")
         return render(request, 'app/index.html', {
             'post_data': post_data,
+        })
+
+
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        return render(request, 'app/post_detail.html', {
+            'post_data': post_data
         })
 
 
@@ -25,12 +33,12 @@ class CreatePostView(View):
 
         if form.is_valid():
             post_data = Post()
-            post_data.author = request.user
             post_data.title = form.cleaned_data['title']
             post_data.content = form.cleaned_data['content']
             if request.FILES:
                 post_data.image = request.FILES.get('image')
             post_data.save()
+            return redirect('post_detail', post_data.id)
 
         return render(request, 'app/post_form.html', {
             'form': form
