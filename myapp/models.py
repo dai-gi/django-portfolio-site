6b61b9a4bacd21cb8341be3d3174d2fc1469e33a
dai-gi <daisuke.u524@gmail.com>
2021-02-19 06:46:09 +0900

    追加

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Post(models.Model):
-    image = models.ImageField('画像', upload_to='img', null=True, blank=True)
+    image = models.ImageField('イメージ画像', upload_to='img', null=True, blank=True)
     title = models.CharField("タイトル", max_length=100)
     content = models.TextField("内容")
+    url = models.URLField('URL', max_length=400, null=True, blank=True)
 
     def __str__(self):
         return self.title
