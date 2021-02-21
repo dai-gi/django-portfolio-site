093a3af03a73ab1a1754d327383c7c8663b7105d
dai-gi <daisuke.u524@gmail.com>
2021-02-21 12:56:18 +0900

    削除

--- myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class PostForm(forms.Form):
-    image = forms.ImageField(label='イメージ画像', required=False)
-    title = forms.CharField(max_length=50, label='タイトル')
-    content = forms.CharField(label='内容', widget=forms.Textarea())
